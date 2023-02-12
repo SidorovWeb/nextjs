1b55c0ec7641a0259eb7aad4b28b41ab6785c070
Alexandr <sidorovweb@gmail.com>
2023-02-12 17:39:17 +0300

    Создание компонента

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <div>Header</div>
+      <nav>
+        <a>home</a>
+        <a>about</a>
+        <a>comments</a>
+        <a>burgers</a>
+      </nav>
+    </header>
+  );
+};
